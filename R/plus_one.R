ca1f73d0d00811c7f1fea6c32088329054d15aef
Timothy Fraser, PhD <timothy.fraser.1@gmail.com>
2023-11-02 09:19:42 -0400

    Create plus_one.R

---
+++ R/plus_one.R
@@ -0,0 +1,11 @@
+#' @name plus_one
+#' @title Plus One
+#' @description Function to add 1 to a numeric input vector.
+#' @author Tim Fraser, PhD
+#' @params x (numeric) vector of 1 or more numeric values.
+#' @note Adding `@export` below means this function will become accessible by package users, rather than being an internal-only function.
+#' @export
+plus_one = function(x){
+  output = x + 1
+  return(output)
+} 
